e2d3c603f56d740ca83312081a5f849fcdc1c181
natalijapavlovic17321 <77362593+natalijapavlovic17321@users.noreply.github.com>
2021-11-29 03:38:15 +0100

    Add files via upload

---
+++ 17321-Zadatak2-ZI/17321-Zadatak1-ZI/Form1.Designer.cs
@@ -0,0 +1,294 @@
+﻿
+namespace _17321_Zadatak1_ZI
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(180, 93);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Kodiraj";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(181, 242);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 29);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Dekodiraj";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.Filter = "*txt";
+            this.fileSystemWatcher1.IncludeSubdirectories = true;
+            this.fileSystemWatcher1.Path = "C:\\Users\\Natalija\\Desktop\\17321-Zadatak1-ZI\\FileSystemWatcher";
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            this.fileSystemWatcher1.Changed += new System.IO.FileSystemEventHandler(this.fileSystemWatcher1_Changed);
+            this.fileSystemWatcher1.Created += new System.IO.FileSystemEventHandler(this.fileSystemWatcher1_Created);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(355, 169);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(94, 29);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Promeni ";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(220, 132);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(502, 27);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(220, 279);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(502, 27);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(220, 209);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(502, 27);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.Text = "C:\\Users\\Natalija\\Desktop\\17321-Zadatak1-ZI\\KodiraniBF";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(59, 102);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Kodiraj fajl :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(59, 246);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Dekodiraj fajl:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(59, 173);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(290, 20);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Promeni putanju cuvanja kodiranih fajlova:";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(59, 12);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(215, 24);
+            this.checkBox1.TabIndex = 10;
+            this.checkBox1.Text = "Ukljucen FileSystemWatcher";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(56, 408);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(547, 20);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "*Da bi promenili putanju,dekodirali ili kodirali fajlove iskljuciti FileSystemWat" +
+    "cher";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(372, 323);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(94, 29);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "Promeni";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(59, 327);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(307, 20);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Promeni putanju cuvanja dekodiranih fajlova:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(220, 358);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(502, 27);
+            this.textBox4.TabIndex = 14;
+            this.textBox4.Text = "C:\\Users\\Natalija\\Desktop\\17321-Zadatak1-ZI\\DekodiraniBF";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(59, 139);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(138, 20);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Poslednje kodirano:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(59, 282);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(155, 20);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Poslednje dekodirano:";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(76, 54);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(160, 24);
+            this.radioButton1.TabIndex = 18;
+            this.radioButton1.Text = "Double transpositin";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Checked = true;
+            this.radioButton2.Location = new System.Drawing.Point(288, 54);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(88, 24);
+            this.radioButton2.TabIndex = 19;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "BlowFish";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(800, 451);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Double transposition";
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+    }
+}
+
---
+++ 17321-Zadatak2-ZI/17321-Zadatak1-ZI/Form1.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Double_trasposition2;
+using System.IO;
+using _17321_Zadatak1_ZI.BlowFish2;
+using System.Security.Cryptography;
+
+namespace _17321_Zadatak1_ZI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void fileSystemWatcher1_Created(object sender, FileSystemEventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                {
+                    if (!IsFileReady(e.FullPath))
+                    { }
+                    else
+                    {
+                        string destination = textBox3.Text;
+                        SelectAlgorithm(e.FullPath, e.Name, destination);
+                    }
+                    }
+                }
+        }
+        public void SelectAlgorithm(string source, string name, string destination)
+        {
+            if (radioButton1.Checked == true)
+            {
+                CodeDT(source, name, destination);
+            }
+            else if (radioButton2.Checked == true)
+            {
+                CodeBF(source, name, destination);
+            }
+        }
+        public void SelectAlgorithmDecode(string source, string name, string destination)
+        {
+            if (radioButton1.Checked == true)
+            {
+                DecodeDT(source, name, destination);
+            }
+            else if (radioButton2.Checked == true)
+            {
+                DecodeBF(source, name, destination);
+            }
+        }
+        public static bool IsFileReady(string filename)
+        { //Solved
+          //Exception The process cannot access the file,because it is being used by another process
+            try
+            {
+                using (FileStream inputStream = File.Open(filename, FileMode.Open, FileAccess.Read, FileShare.None))
+                    return inputStream.Length > 0;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        private void CodeBF(string source, string name, string destination)
+        {
+            string file = File.ReadAllText(source);
+            byte[] hashValue=null;
+            SHA256 mySHA256 = SHA256.Create();
+            try
+            {
+                FileStream fileStream = File.Open(source, FileMode.Open, FileAccess.Read, FileShare.None);
+                fileStream.Position = 0;
+                hashValue = mySHA256.ComputeHash(fileStream);
+                fileStream.Close();
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine($"I/O Exception: {e.Message}");
+            }
+            if(mySHA256.HashSize>448)
+                System.Windows.Forms.MessageBox.Show("Maksimalna duzina kljuca je 56 bajta");
+            BlowFish bf = new BlowFish(hashValue);
+            string text=bf.Code(file);
+
+            string dest = destination + @"\" + name;
+            File.WriteAllText(dest, text);
+            string key = source  + " " + bf.ByteToHex(hashValue) + " " + dest + "\n";
+            string pathKey = @"C:\Users\Natalija\Desktop\17321-Zadatak1-ZI\KeysBF.txt";
+            File.AppendAllText(pathKey, key);
+        }
+        private void DecodeBF(string source, string name, string destination)
+        {
+            string file = File.ReadAllText(source);
+            string pathKey = @"C:\Users\Natalija\Desktop\17321-Zadatak1-ZI\KeysBF.txt";
+            string key = null ;
+            foreach (string line in File.ReadLines(pathKey))
+            {
+
+                if (line.EndsWith(source))
+                {
+                    string[] keys = line.Split(" ");
+                    key= keys[1];
+                }
+            }
+            BlowFish bf = new BlowFish(key);
+            string text = bf.Decode(file);
+            string dest = destination + @"\" + name;
+            File.WriteAllText(dest, text);
+        }
+        private void CodeDT(string source, string name, string destination)
+        {
+            string file = null;
+            file = File.ReadAllText(source);
+            string[] text = new string[3];
+            Double_trasposition dt = new Double_trasposition(file);
+            text = dt.Code(file);
+
+            string dest = destination + @"\" + name;
+            File.WriteAllText(dest, text[0]);
+            string key = source + " " + text[1] + " " + text[2] +" "+ dest + "\n";
+            string pathKey = @"C:\Users\Natalija\Desktop\17321-Zadatak1-ZI\KeysDT.txt";
+            File.AppendAllText(pathKey, key);
+        }
+        private void DecodeDT(string source, string name, string destination)
+        {
+            string text,keyM="",keyN="";
+            string file = File.ReadAllText(source);
+            Double_trasposition dt = new Double_trasposition(file);
+            
+            string pathKey = @"C:\Users\Natalija\Desktop\17321-Zadatak1-ZI\KeysDT.txt";
+            foreach (string line in File.ReadLines(pathKey))
+            {
+
+                if (line.EndsWith(source))
+                {
+                    string[] keys = line.Split(" ");
+                    keyM = keys[1];
+                    keyN = keys[2];
+                }
+            }
+            text = dt.Decode(file,keyM,keyN);
+            string dest = destination + @"\" + name;
+            File.WriteAllText(dest, text);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog fbd = new FolderBrowserDialog())
+            {
+                if (fbd.ShowDialog() == DialogResult.OK)
+                {
+                    textBox3.Text = fbd.SelectedPath;
+                }
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                button1.Enabled = false;
+                textBox1.Enabled = false;
+                button2.Enabled = false;
+                textBox2.Enabled = false;
+                button3.Enabled = false;
+                textBox3.Enabled = false;
+                button4.Enabled = false;
+                textBox4.Enabled = false;
+            }
+            else
+            {
+                button1.Enabled = true;
+                textBox1.Enabled = true;
+                button2.Enabled = true;
+                textBox2.Enabled = true;
+                button3.Enabled = true;
+                textBox3.Enabled = true;
+                button4.Enabled = true;
+                textBox4.Enabled = true;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    if (new FileInfo(ofd.FileName).Length == 0) //nema sta da dekodira
+                    {
+                        return;
+                    }
+                    textBox2.Text = ofd.FileName;
+                    SelectAlgorithmDecode(ofd.FileName, Path.GetFileName(ofd.FileName), textBox4.Text);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    if (new FileInfo(ofd.FileName).Length == 0) //nema sta da kodira
+                    {
+                        return;
+                    }
+                    textBox1.Text = ofd.FileName;
+                    deleteFromKeys(ofd.FileName); //ukoliko se kodira opet sta je kodirano/ postoji vrednost kljuceva za taj fajl
+                                                  //obrisace se prethodne vrednosti
+                                                  //Code(ofd.FileName, Path.GetFileName(ofd.FileName), textBox3.Text);
+
+                    SelectAlgorithm(ofd.FileName, Path.GetFileName(ofd.FileName), textBox3.Text);
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog fbd = new FolderBrowserDialog())
+            {
+                if (fbd.ShowDialog() == DialogResult.OK)
+                {
+                    textBox4.Text = fbd.SelectedPath;
+                }
+            }
+        }
+        private void deleteFromKeys(string FullPath)
+        {
+            string pathKeys;
+            if (radioButton1.Checked)
+                pathKeys = @"C:\Users\Natalija\Desktop\17321-Zadatak1-ZI\KeysDT.txt";
+            else pathKeys = @"C:\Users\Natalija\Desktop\17321-Zadatak1-ZI\KeysBF.txt";
+            var tempFile = Path.GetTempFileName();
+            var linesToKeep = File.ReadLines(pathKeys).Where(l => !l.StartsWith(FullPath));
+            File.WriteAllLines(tempFile, linesToKeep);
+            File.Delete(pathKeys);
+            File.Move(tempFile, pathKeys);
+        }
+      private void fileSystemWatcher1_Changed(object sender, FileSystemEventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                {
+                    if (!IsFileReady(e.FullPath))
+                    { }
+                    else
+                    {
+                        deleteFromKeys(e.FullPath);
+                        string destination = textBox3.Text;
+                        SelectAlgorithm(e.FullPath, e.Name, destination);
+                    }
+                }
+            }
+        }
+    }
+}

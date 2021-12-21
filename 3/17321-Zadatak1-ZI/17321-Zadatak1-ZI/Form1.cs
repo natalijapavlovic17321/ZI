d55208291f34f38ca0f681a07046ea8f33288828
natalijapavlovic17321 <77362593+natalijapavlovic17321@users.noreply.github.com>
2021-12-21 02:06:12 +0100

    Add files via upload

---
+++ 3/17321-Zadatak1-ZI/17321-Zadatak1-ZI/Form1.cs
@@ -0,0 +1,336 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Double_trasposition2;
+using System.IO;
+using _17321_Zadatak1_ZI.BlowFish2;
+using _17321_Zadatak1_ZI.MD5;
+using System.Security.Cryptography;
+
+namespace _17321_Zadatak1_ZI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void fileSystemWatcher1_Created(object sender, FileSystemEventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                {
+                    if (!IsFileReady(e.FullPath))
+                    { }
+                    else
+                    {
+                        string destination = textBox3.Text;
+                        SelectAlgorithm(e.FullPath, e.Name, destination);
+                    }
+                    }
+                }
+        }
+        public void SelectAlgorithm(string source, string name, string destination)
+        {
+            if (radioButton1.Checked == true)
+            {
+                CodeDT(source, name, destination);
+            }
+            else if (radioButton2.Checked == true)
+            {
+                CodeBF(source, name, destination);
+            }
+        }
+        public void SelectAlgorithmDecode(string source, string name, string destination)
+        {
+            if (radioButton1.Checked == true)
+            {
+                DecodeDT(source, name, destination);
+            }
+            else if (radioButton2.Checked == true)
+            {
+                DecodeBF(source, name, destination);
+            }
+        }
+        public static bool IsFileReady(string filename)
+        { //Solved
+          //Exception The process cannot access the file,because it is being used by another process
+            try
+            {
+                using (FileStream inputStream = File.Open(filename, FileMode.Open, FileAccess.Read, FileShare.None))
+                    return inputStream.Length > 0;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        private void CodeBF(string source, string name, string destination)
+        {
+            string file = File.ReadAllText(source);
+            byte[] hashValue=null;
+            SHA256 mySHA256 = SHA256.Create();
+            try
+            {
+                FileStream fileStream = File.Open(source, FileMode.Open, FileAccess.Read, FileShare.None);
+                fileStream.Position = 0;
+                hashValue = mySHA256.ComputeHash(fileStream);
+                fileStream.Close();
+            }
+            catch (IOException e)
+            {
+                System.Windows.Forms.MessageBox.Show(e.Message);
+            }
+            if(mySHA256.HashSize>448)
+                System.Windows.Forms.MessageBox.Show("Maksimalna duzina kljuca je 56 bajta");
+            BlowFish bf = new BlowFish(hashValue);
+            string text=bf.Code(file);
+
+            Md5 MD5 = new Md5();
+            string hashCode = MD5.GetHash(file); //hash
+
+            string write = hashCode + "\n" + text; //upisuje u fajl 
+                                                   //prva linija hash
+
+            string dest = destination + @"\" + name;
+            File.WriteAllText(dest, write);
+            string key = source  + " " + bf.ByteToHex(hashValue) + " " + dest + "\n";
+            string pathKey = @"C:\Users\Natalija\Desktop\17321-Zadatak1-ZI\KeysBF.txt";
+            File.AppendAllText(pathKey, key);
+        }
+        private void DecodeBF(string source, string name, string destination)
+        {
+            try
+            {
+                //string file = File.ReadAllText(source);
+                //string hash1 = File.ReadLines(source).First(); // hash
+
+                string[] lines = File.ReadAllLines(source);
+                string textDecode = "";
+                for (int i = 1; i < lines.Length; i++)
+                {
+                    textDecode += lines[i]; //sve ostale linije sem hash-a
+                }
+                string hash1 = lines[0]; //hash
+
+                string pathKey = @"C:\Users\Natalija\Desktop\17321-Zadatak1-ZI\KeysBF.txt";
+                string key = null;
+                foreach (string line in File.ReadLines(pathKey))
+                {
+
+                    if (line.EndsWith(source))
+                    {
+                        string[] keys = line.Split(" ");
+                        key = keys[1];
+                    }
+                }
+                BlowFish bf = new BlowFish(key);
+                string text = bf.Decode(textDecode);
+
+                Md5 MD5 = new Md5();
+                string hash2 = MD5.GetHash(text); //hash dekodiranog teksta
+
+                if (hash1 != hash2)
+                    MessageBox.Show("Different hash codes");
+                else
+                {
+                    string dest = destination + @"\" + name;
+                    File.WriteAllText(dest, text);
+                    MessageBox.Show("Same hash codes");
+                }
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show("Try different encryption/decryption algorythm. \n" +
+                    "Error: " + e + "\n");
+            }
+        }
+        private void CodeDT(string source, string name, string destination)
+        {
+            string file = null;
+            file = File.ReadAllText(source);
+            string[] text = new string[3];
+            Double_trasposition dt = new Double_trasposition(file);
+            text = dt.Code(file); //kodirani tekst
+
+            Md5 MD5 = new Md5();
+            string hashCode=MD5.GetHash(file); //hash
+
+            string write = hashCode + "\n" + text[0]; //upisuje u fajl 
+                                                   //prva linija hash
+
+            string dest = destination + @"\" + name;
+            File.WriteAllText(dest, write);
+            string key = source + " " + text[1] + " " + text[2] +" "+ dest + "\n";
+            string pathKey = @"C:\Users\Natalija\Desktop\17321-Zadatak1-ZI\KeysDT.txt";
+            File.AppendAllText(pathKey, key);
+        }
+        private void DecodeDT(string source, string name, string destination)
+        {
+            try
+            {
+                //string file = File.ReadAllText(source);
+                //string hash1 = File.ReadLines(source).First(); // hash
+
+                string[] lines = File.ReadAllLines(source);
+                string textDecode = "";
+                for (int i = 1; i < lines.Length; i++)
+                {
+                    textDecode = textDecode + lines[i]; //sve ostale linije sem hash-a
+                }
+                string hash1 = lines[0]; //hash
+
+                string text, keyM = "", keyN = "";
+                Double_trasposition dt = new Double_trasposition(textDecode);
+
+                string pathKey = @"C:\Users\Natalija\Desktop\17321-Zadatak1-ZI\KeysDT.txt";
+                foreach (string line in File.ReadLines(pathKey))
+                {
+
+                    if (line.EndsWith(source))
+                    {
+                        string[] keys = line.Split(" ");
+                        keyM = keys[1];
+                        keyN = keys[2];
+                    }
+                }
+                text = dt.Decode(textDecode, keyM, keyN);
+
+                Md5 MD5 = new Md5();
+                string hash2 = MD5.GetHash(text); //hash dekodiranog teksta
+
+                if (hash1 != hash2)
+                    MessageBox.Show("Different hash codes");
+                else
+                {
+                    string dest = destination + @"\" + name;
+                    File.WriteAllText(dest, text);
+                    MessageBox.Show("Same hash codes");
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Try different encryption/decryption algorythm. \n" +
+                    "Error: " + e + "\n" );
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog fbd = new FolderBrowserDialog())
+            {
+                if (fbd.ShowDialog() == DialogResult.OK)
+                {
+                    textBox3.Text = fbd.SelectedPath;
+                }
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                button1.Enabled = false;
+                textBox1.Enabled = false;
+                button2.Enabled = false;
+                textBox2.Enabled = false;
+                button3.Enabled = false;
+                textBox3.Enabled = false;
+                button4.Enabled = false;
+                textBox4.Enabled = false;
+            }
+            else
+            {
+                button1.Enabled = true;
+                textBox1.Enabled = true;
+                button2.Enabled = true;
+                textBox2.Enabled = true;
+                button3.Enabled = true;
+                textBox3.Enabled = true;
+                button4.Enabled = true;
+                textBox4.Enabled = true;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    if (new FileInfo(ofd.FileName).Length == 0) //nema sta da dekodira
+                    {
+                        return;
+                    }
+                    textBox2.Text = ofd.FileName;
+                    SelectAlgorithmDecode(ofd.FileName, Path.GetFileName(ofd.FileName), textBox4.Text);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    if (new FileInfo(ofd.FileName).Length == 0) //nema sta da kodira
+                    {
+                        return;
+                    }
+                    textBox1.Text = ofd.FileName;
+                    deleteFromKeys(ofd.FileName); //ukoliko se kodira opet sta je kodirano/ postoji vrednost kljuceva za taj fajl
+                                                  //obrisace se prethodne vrednosti
+                                                  //Code(ofd.FileName, Path.GetFileName(ofd.FileName), textBox3.Text);
+
+                    SelectAlgorithm(ofd.FileName, Path.GetFileName(ofd.FileName), textBox3.Text);
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog fbd = new FolderBrowserDialog())
+            {
+                if (fbd.ShowDialog() == DialogResult.OK)
+                {
+                    textBox4.Text = fbd.SelectedPath;
+                }
+            }
+        }
+        private void deleteFromKeys(string FullPath)
+        {
+            string pathKeys;
+            if (radioButton1.Checked)
+                pathKeys = @"C:\Users\Natalija\Desktop\17321-Zadatak1-ZI\KeysDT.txt";
+            else pathKeys = @"C:\Users\Natalija\Desktop\17321-Zadatak1-ZI\KeysBF.txt";
+            var tempFile = Path.GetTempFileName();
+            var linesToKeep = File.ReadLines(pathKeys).Where(l => !l.StartsWith(FullPath));
+            File.WriteAllLines(tempFile, linesToKeep);
+            File.Delete(pathKeys);
+            File.Move(tempFile, pathKeys);
+        }
+      private void fileSystemWatcher1_Changed(object sender, FileSystemEventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                {
+                    if (!IsFileReady(e.FullPath))
+                    { }
+                    else
+                    {
+                        deleteFromKeys(e.FullPath);
+                        string destination = textBox3.Text;
+                        SelectAlgorithm(e.FullPath, e.Name, destination);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 3/17321-Zadatak1-ZI/17321-Zadatak1-ZI/MD5/Md5.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _17321_Zadatak1_ZI.BlowFish2;
+using System.Security.Cryptography;
+
+namespace _17321_Zadatak1_ZI.MD5
+{
+    class Md5
+    {
+        private byte[] text;
+        private int[] r;
+        private uint[] k;
+        private uint h1;
+        private uint h2;
+        private uint h3;
+        private uint h0;
+
+        #region Constructors
+        public Md5()
+        {
+            k = new uint[64];
+            set();
+        }
+        public Md5(string text)
+        {
+            k = new uint[64];
+            this.text = Encoding.ASCII.GetBytes(text);
+            set();
+        }
+        public void set()
+        {
+            r = new int[64]{ 7, 12, 17, 22,  7, 12, 17, 22,  7, 12, 17, 22,  7, 12, 17, 22,
+            5,  9, 14, 20,  5,  9, 14, 20,  5,  9, 14, 20,  5,  9, 14, 20,
+            4, 11, 16, 23,  4, 11, 16, 23,  4, 11, 16, 23,  4, 11, 16, 23,
+            6, 10, 15, 21,  6, 10, 15, 21,  6, 10, 15, 21,  6, 10, 15, 21
+            };
+
+            for (int i = 0; i < 64; i++)
+                k[i] = Convert.ToUInt32(Math.Floor((Math.Abs(Math.Sin(i + 1)))* Math.Pow(2,32)));
+
+            h0 = 0x67452301;
+            h1 = 0xEFCDAB89;
+            h2 = 0x98BADCFE;
+            h3 = 0x10325476;
+        }
+        #endregion Constructors
+
+        #region HashAlgorithm
+        public string GetHash(string text)
+        {
+            this.text = Encoding.ASCII.GetBytes(text);
+            uint a0 = h0;   // A
+            uint b0 = h1;   // B
+            uint c0 = h2;   // C
+            uint d0 = h3;   // D
+
+            var newLength = (56 - ((this.text.Length + 1) % 64)) % 64; // length= append "0" bits until message length in bits ≡ 448 (mod 512)
+            byte[] newText= new byte[this.text.Length + 1 + newLength + 8];
+            Array.Copy(this.text, newText, this.text.Length);
+            newText[this.text.Length] = 0x80; // append "1" bit to message
+
+            byte[] length = BitConverter.GetBytes(this.text.Length * 8); // little endian reči   
+            Array.Copy(length, 0, newText, newText.Length - 8, 4); // add length in bits
+
+            for (int i = 0; i < newText.Length / 64; ++i) //po 512 bitova
+            {
+                uint[] w = new uint[16]; //niz od po 16 little endian reči   
+                for (int j = 0; j < 16; ++j)
+                     w[j] = BitConverter.ToUInt32(newText, (i * 64) + (j * 4));
+
+                uint A = a0, B = b0, C = c0, D = d0, f = 0, g = 0;
+                // Glavna petlja:
+                for (uint y = 0; y < 64; ++y)
+                {
+                    if (y>=0 && y <= 15)
+                    {
+                        f = (B & C) | (~B & D);
+                        g = y;
+                    }
+                    else if (y >= 16 && y <= 31)
+                    {
+                        f = (D & B) | (~D & C);
+                        g = ((5 * y) + 1) % 16;
+                    }
+                    else if (y >= 32 && y <= 47)
+                    {
+                        f = B ^ C ^ D;
+                        g = ((3 * y) + 5) % 16;
+                    }
+                    else if (y >= 48)
+                    {
+                        f = C ^ (B | ~D);
+                        g = (7 * y) % 16;
+                    }
+                    var temp = D;
+                    D = C;
+                    C = B;
+                    B = B + leftRotate((A + f + k[y] + w[g]), r[y]);
+                    A = temp;
+                }
+                a0 += A;
+                b0 += B;
+                c0 += C;
+                d0 += D;
+            }
+            return (ByteToString(a0) + ByteToString(b0) + ByteToString(c0) + ByteToString(d0));
+        }
+        #endregion HashAlgorithm
+
+        #region Functions
+
+        public uint leftRotate(uint x, int c)
+        {
+            return (x << c) | (x >> (32 - c));
+        }
+        private static string ByteToString(uint x)
+        {
+            return String.Join("", BitConverter.GetBytes(x).Select(y => y.ToString("x2")));
+            //return UInt32.ToString()
+        }
+
+        #endregion Functions
+    }
+}
